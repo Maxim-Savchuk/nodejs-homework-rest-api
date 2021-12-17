ee0c600e8a7c0dfcab0b20d296c7c904be9f6633
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-12-18 00:16:51 +0200

    Added avatar processing

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,6 +1,7 @@
-const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
+const jimp = require("jimp");
+const { User } = require("../../models");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -12,6 +13,12 @@ const updateAvatar = async (req, res) => {
     const resultUpload = path.join(avatarsDir, imageName);
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("public", "avatars", imageName);
+    jimp
+      .read(avatarURL)
+      .then((image) => {
+        return image.resize(250, 250).write(avatarURL);
+      })
+      .catch((err) => console.log(err));
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.json({ avatarURL });
   } catch (error) {
